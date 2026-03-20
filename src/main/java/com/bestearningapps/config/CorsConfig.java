ef47b2c72edcdea4aab60dc08aa629e6e45c131e
Shashi Gaurav <shashigaurav789@gmail.com>
2026-03-20 23:17:05 +0530

    url add

--- src/main/java/com/bestearningapps/config/CorsConfig.java
+++ src/main/java/com/bestearningapps/config/CorsConfig.java
@@ -10,11 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://best-earning-apps.netlify.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-
     }
-
 }
